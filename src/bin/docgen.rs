0738ed124650fed1bcaa220e4932a762919c6661
sebastienValla <sebastien.valla09@gmail.com>
2024-11-07 16:18:31 +0100

    update docgen

--- src/bin/docgen.rs
+++ src/bin/docgen.rs
@@ -3,35 +3,37 @@ use cairo_doc_gen::parser::parse_cairo_comments;
 use std::fs::{self, File};
 use std::io::Read;
 use std::path::Path;
+use walkdir::WalkDir;
 
 fn main() -> std::io::Result<()> {
     // Vector to hold all FunctionDocs
     let mut all_docs = Vec::new();
 
     // Path to the src directory
-    let src_path = "./src"; // Looks in the "src" directory within the root
+    let src_path = "../src"; // Base directory to start searching for .cairo files
 
-    // Iterate over all .cairo files in the src directory
-    for entry in fs::read_dir(src_path)? {
-        let entry = entry?;
+    // Recursively traverse all files in the src directory and subdirectories
+    for entry in WalkDir::new(src_path)
+        .into_iter()
+        .filter_map(Result::ok) // Filter out errors
+        .filter(|e| e.path().extension().and_then(|s| s.to_str()) == Some("cairo"))
+    {
         let path = entry.path();
+        let filename = path.file_name().unwrap().to_string_lossy().to_string();
 
-        // Only process .cairo files
-        if path.extension().and_then(|s| s.to_str()) == Some("cairo") {
-            let filename = path.file_name().unwrap().to_string_lossy().to_string();
-            let mut file = File::open(&path)?;
-            let mut file_content = String::new();
-            file.read_to_string(&mut file_content)?;
+        // Open and read each .cairo file
+        let mut file = File::open(&path)?;
+        let mut file_content = String::new();
+        file.read_to_string(&mut file_content)?;
 
-            // Parse the comments in the file
-            let docs = parse_cairo_comments(&file_content, &filename);
+        // Parse the comments in the file
+        let docs = parse_cairo_comments(&file_content, &filename);
 
-            // Collect the docs
-            all_docs.extend(docs);
-        }
+        // Collect the docs
+        all_docs.extend(docs);
     }
 
-    // Generate HTML documentation from all collected docs
+    // Generate HTML documentation from all docs
     let html = generate_html(all_docs).expect("Error generating HTML");
 
     // Ensure the documentation directory exists at the project root
